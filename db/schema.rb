00b8ce218162c86ce73c390d363c5c4ca622404b
Naomi Flagg <neffly511@gmail.com>
2021-02-01 09:53:57 -0800

    Create index for players table position column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_230744) do
+ActiveRecord::Schema.define(version: 2021_02_01_175129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_01_17_230744) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "position"
     t.integer "prediction"
+    t.index ["position"], name: "index_players_on_position"
     t.index ["team_id"], name: "index_players_on_team_id"
   end
 
