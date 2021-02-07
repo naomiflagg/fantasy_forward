cfc2e384b6b07f851deddcab2e1778170565da4b
Naomi Flagg <neffly511@gmail.com>
2021-02-07 11:03:16 -0800

    Add selection methods and tests for all positions

--- lib/team_optimizer.rb
+++ lib/team_optimizer.rb
@@ -13,10 +13,27 @@ class TeamOptimizer
     scores = possible_teams.map { |possible_team| estimate_score(possible_team) }
     scores.sort.first
   end
+  
+  # Refactor needed - turn below four into one function that takes arguments for position and number of players.
 
   def self.select_goalie
     goalie_benchmark = Player.where(position: 'Goalkeeper').order(prediction: :desc).first.prediction
     Player.where(position: 'Goalkeeper').where('prediction >= ?', goalie_benchmark)
+  end
+
+  def self.select_defenders
+    defender_benchmark = Player.where(position: 'Defender').order(prediction: :desc).fifth.prediction
+    Player.where(position: 'Defender').where('prediction >= ?', defender_benchmark)
+  end
+
+  def self.select_midfielders
+    mid_benchmark = Player.where(position: 'Midfielder').order(prediction: :desc).fifth.prediction
+    Player.where(position: 'Midfielder').where('prediction >= ?', mid_benchmark)
+  end
+
+  def self.select_forwards
+    forward_benchmark = Player.where(position: 'Forward').order(prediction: :desc).third.prediction
+    Player.where(position: 'Forward').where('prediction >= ?', forward_benchmark)
   end
 
   def self.enumerate_possible_teams(goalie, defenders, mids, forwards)
--- spec/lib/team_optimizer_spec.rb
+++ spec/lib/team_optimizer_spec.rb
@@ -35,17 +35,115 @@ RSpec.describe 'team_optimizer' do
   end
 
   describe '#select_defenders' do
-    it 'selects top 5 defenders' do
+    before(:all) do
+      @defender1 = create(:player, position: 'Defender', prediction: 101)
+      @defender2 = create(:player, position: 'Defender', prediction: 100)
+      @defender3 = create(:player, position: 'Defender', prediction: 99)
+      @defender4 = create(:player, position: 'Defender', prediction: 102)
+      @defender5 = create(:player, position: 'Defender', prediction: 101)
+      @defender6 = create(:player, position: 'Defender', prediction: 98)
+      @defenders = TeamOptimizer.select_defenders
+    end
+
+    context 'when there is no tie in scores' do
+      it 'selects only 5' do
+        expect(@defenders.count).to eq(5)
+      end
+
+      it 'selects top 5' do
+        expect(@defenders.include?(@defender1)).to be true
+        expect(@defenders.include?(@defender2)).to be true
+        expect(@defenders.include?(@defender3)).to be true
+        expect(@defenders.include?(@defender4)).to be true
+        expect(@defenders.include?(@defender5)).to be true
+      end
+    end
+
+    context 'when fifth top sore is shared' do
+      it 'selects all defenders with that score' do
+        defender7 = create(:player, position: 'Defender', prediction: 99)
+        defenders = TeamOptimizer.select_defenders
+        expect(defenders.include?(defender7)).to be true
+        expect(defenders.count).to eq(6)
+      end
+    end
+
+    it 'does not include defender with lower score' do
+      expect(@defenders.include?(@defender6)).to be false
     end
   end
 
   describe '#select_midfielders' do
-    it 'selects top 5 midfielders' do
+    before(:all) do
+      @mid1 = create(:player, position: 'Midfielder', prediction: 101)
+      @mid2 = create(:player, position: 'Midfielder', prediction: 99)
+      @mid3 = create(:player, position: 'Midfielder', prediction: 102)
+      @mid4 = create(:player, position: 'Midfielder', prediction: 102)
+      @mid5 = create(:player, position: 'Midfielder', prediction: 101)
+      @mid6 = create(:player, position: 'Midfielder', prediction: 98)
+      @mids = TeamOptimizer.select_midfielders
+    end
+
+    context 'when there is no tie in scores' do
+      it 'selects only 5' do
+        expect(@mids.count).to eq(5)
+      end
+
+      it 'selects top 5' do
+        expect(@mids.include?(@mid1)).to be true
+        expect(@mids.include?(@mid2)).to be true
+        expect(@mids.include?(@mid3)).to be true
+        expect(@mids.include?(@mid4)).to be true
+        expect(@mids.include?(@mid5)).to be true
+      end
+    end
+
+    context 'when fifth top sore is shared' do
+      it 'selects all defenders with that score' do
+        mid7 = create(:player, position: 'Midfielder', prediction: 99)
+        mids = TeamOptimizer.select_midfielders
+        expect(mids.include?(mid7)).to be true
+        expect(mids.count).to eq(6)
+      end
+    end
+
+    it 'does not include defender with lower score' do
+      expect(@mids.include?(@mid6)).to be false
     end
   end
 
   describe '#select_forwards do' do
-    it 'selects top 3 forwards do' do
+    before(:all) do
+      @forward1 = create(:player, position: 'Forward', prediction: 101)
+      @forward2 = create(:player, position: 'Forward', prediction: 100)
+      @forward3 = create(:player, position: 'Forward', prediction: 101)
+      @forward4 = create(:player, position: 'Forward', prediction: 99)
+      @forwards = TeamOptimizer.select_forwards
+    end
+
+    context 'when there is no tie in scores' do
+      it 'selects only 3' do
+        expect(@forwards.count).to eq(3)
+      end
+
+      it 'selects top 3' do
+        expect(@forwards.include?(@forward1)).to be true
+        expect(@forwards.include?(@forward2)).to be true
+        expect(@forwards.include?(@forward3)).to be true
+      end
+    end
+
+    context 'when fifth top sore is shared' do
+      it 'selects all defenders with that score' do
+        forward5 = create(:player, position: 'Forward', prediction: 100)
+        forwards = TeamOptimizer.select_forwards
+        expect(forwards.include?(forward5)).to be true
+        expect(forwards.count).to eq(4)
+      end
+    end
+
+    it 'does not include defender with lower score' do
+      expect(@forwards.include?(@forward4)).to be false
     end
   end
 end
