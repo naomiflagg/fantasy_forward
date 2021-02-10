292647ae9582093a67b5bf43cc58ec62e7a3fe8d
Naomi Flagg <neffly511@gmail.com>
2021-02-10 09:59:48 -0800

    Update time for daily cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,6 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-
-every 1.day do
+every :day, at: '10:00 PM' do
   rake 'db:seed'
 end
