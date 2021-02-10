9467be2f5a8be395912f26f5823d1ab0d14e8ee4
Naomi Flagg <neffly511@gmail.com>
2021-02-09 20:59:26 -0800

    Add player DB wiping before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require 'httparty'
 require 'json'
 
 # Run once before season begins
-def teams
+def seed_teams
   Team.destroy_all
   response = HTTParty.get('https://fantasy.premierleague.com/api/bootstrap-static/')
   response.parsed_response
@@ -17,7 +17,8 @@ def teams
   end
 end
 
-def players
+def seed_players
+  Player.destroy_all
   positions = { 1 => 'Goalkeeper', 2 => 'Defender', 3 => 'Midfielder', 4 => 'Forward' }
   response = HTTParty.get('https://fantasy.premierleague.com/api/bootstrap-static/')
   response.parsed_response
@@ -25,13 +26,19 @@ def players
     puts 'error seeding players'
   else
     response['elements'].each do |element|
-      f_name = element['first_name'].to_s
-      l_name = element['second_name'].to_s
-      player = Player.where(first_name: f_name, last_name: l_name).first_or_initialize
-      player.update!(goals: element['goals_scored'], assists: element['assists'],
-                     clean_sheets: element['clean_sheets'], points: element['total_points'], position: (positions[element['element_type']]).to_s, prediction: element['ep_next'], team: Team.find_by(code: (element['team_code']).to_s))
+      Player.create do |p|
+        p.first_name = element['first_name'].to_s
+        p.goals = element['goals_scored']
+        p.assists = element['assists']
+        p.last_name = element['second_name'].to_s
+        p.clean_sheets = element['clean_sheets']
+        p.points = element['total_points']
+        p.position = (positions[element['element_type']]).to_s
+        p.prediction = element['ep_next']
+        p.team = Team.find_by(code: (element['team_code']).to_s)
+      end
     end
   end
 end
 
-players
+seed_players
